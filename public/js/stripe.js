38c260f1d54bd5f5c5732f5c35e45abe3ac31da6
Divyesh Sorthi <divusorthi@gmail.com>
2022-03-02 17:11:27 +0530

    prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,10 +6,10 @@ const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
 export const bookTour = async ntourId =>{
   try{
        // 1) Get checkout session from API
-  const session = await axios(`http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+  const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`
   );
 
-  console.log(session);
+  //console.log(session);
 
 
   // 2) Create checkout from + charge credit card
